2c9dedc70f738ff1d93112624608c8f8f52000dc
Vinicius Franco da Cunha Arantes <viniciusf.arantes@gmail.com>
2019-05-27 16:04:24 -0300

    step 3

--- src/withLoading.js
+++ src/withLoading.js
@@ -1,28 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function withLoading(WrappedComponent, {url}) {
   
   const API_URL = 'http://localhost:3001/'
-  return class extends React.Component {
-    state = {
-      data: [],
-      errorMessage: ''
-    }
-    componentDidMount() {
+  
+  return function() {
+    const [data, setData] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+
+    useEffect(() => {
       setTimeout(() => {
         fetch(API_URL + url).then(response => response.json())
-          .then(data => this.setState({data}))
-          .catch(e => this.setState({errorMessage: 'Houve um erro ao buscar os dados'}))
+          .then(data => setData(data))
+          .catch(e => setErrorMessage('Houve um erro ao buscar os dados'))
           
       }, 2000)
-    }
+    },[])
 
-    render() {
-      if(this.state.data.length > 0) {
-        return(<WrappedComponent data={this.state.data}/>)
-      } else {
-        return(<span>{this.state.errorMessage || 'Loading...'}</span>)
-      }
+    if(data.length > 0) {
+      return(<WrappedComponent data={data}/>)
+    } else {
+      return(<span>{errorMessage || 'Loading...'}</span>)
     }
   }
 }
