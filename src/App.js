d54f4694f25fe8b6ba9ffa1a314f38aafef2e6ff
Vinicius Franco da Cunha Arantes <viniciusf.arantes@gmail.com>
2019-05-29 10:49:12 -0300

    Adjust css

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <span className="App-subtitle">ReactBSB</span>
         <List/>
       </header>
     </div>
